37caf64d6dcafd57e63e9718a8beb17d708f3491
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-06 01:50:56 -0300

    Arrow Componente

---
+++ 01_fundamentos/src/componentes/ArrowComponente.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default ( ) => <div>
+    <h2>Arrow Componente!</h2>
+</div>
+
--- 01_fundamentos/src/index.js
+++ 01_fundamentos/src/index.js
@@ -1,11 +1,13 @@
  import React from 'react'
  import ReactDom from 'react-dom'
  import PrimeiroComponente from './componentes/PrimeiroComponente'
+ import ArrowComponente from './componentes/ArrowComponente'
 
 const elemento = document.getElementById( 'root' )
 
 ReactDom.render( 
     <div>
         <PrimeiroComponente />
+        <ArrowComponente />
     </div>    
 , elemento )
