0a87cd3961e474c3bdf5051c2e9c6e30dbc822a1
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-06 01:21:14 -0300

    Aula2

--- 01_fundamentos/src/index.js
+++ 01_fundamentos/src/index.js
@@ -1,6 +1,14 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+ import React from 'react'
+ import ReactDom from 'react-dom'
 
 const elemento = document.getElementById( 'root' )
 
-ReactDom.render( <h1>Olá Mundo React</h1>, elemento )
+// ReactDom.render( <h1>Olá Mundo React</h1>, elemento )
+
+ReactDom.render( 
+    <ul>
+        <li>1) Pedro</li>
+        <li>2) Maria</li>
+        <li>3) Ana</li>
+    </ul>    
+, elemento )
