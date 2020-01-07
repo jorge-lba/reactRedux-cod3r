a7f6567b251f6639058a9d7e4e0a8b5fbb593c85
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-07 00:04:10 -0300

    Relaçao entre Componentes 2

--- 01_fundamentos/src/index.js
+++ 01_fundamentos/src/index.js
@@ -5,13 +5,20 @@
 //  import Props from './componentes/props'
 //  import { CompA, CompB } from './componentes/doisComponentes'
 //  import MultElementos from './componentes/multElementos'
-import FamiliaSilva from './componentes/familiaSilva'
+// import FamiliaSilva from './componentes/familiaSilva'
+import Familia from './componentes/familia'
+import Membro from './componentes/membro'
+
 
 const elemento = document.getElementById( 'root' )
 
 ReactDom.render( 
     <div>
-        <FamiliaSilva/>
+        <Familia>
+            <Membro nome = 'Andre' sobrenome = 'Pereira' />
+            <Membro nome = 'Mariana' sobrenome = 'Pereira' />
+        </Familia>
+        {/* <FamiliaSilva/> */}
         {/* <PrimeiroComponente /> */}
         {/* <ArrowComponente /> */}
         {/* <Props valor="Bom Dia" aBcD={ Math.pow( 2, 8 ) }/> */}
