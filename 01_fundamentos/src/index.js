a5cf65603ffd00d1bc599dc373fb9d7a0bd64277
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-06 23:08:59 -0300

    Props

--- 01_fundamentos/src/index.js
+++ 01_fundamentos/src/index.js
@@ -2,6 +2,7 @@
  import ReactDom from 'react-dom'
  import PrimeiroComponente from './componentes/PrimeiroComponente'
  import ArrowComponente from './componentes/ArrowComponente'
+ import Props from './componentes/props'
 
 const elemento = document.getElementById( 'root' )
 
@@ -9,5 +10,6 @@ ReactDom.render(
     <div>
         <PrimeiroComponente />
         <ArrowComponente />
+        <Props valor="Bom Dia" aBcD={ Math.pow( 2, 8 ) }/>
     </div>    
 , elemento )
