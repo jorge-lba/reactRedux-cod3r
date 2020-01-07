54c9e6fe33e0c62331f61b18c0ddcbf00c021923
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-07 20:33:32 -0300

    Resolvendo funçao .this - Contador 02

--- 01_fundamentos/src/componentes/contador/contador.jsx
+++ 01_fundamentos/src/componentes/contador/contador.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 
 export default class Contador extends Component {
-    maisUm( ) {
+
+
+    maisUm = ( ) => {
         this.props.numero++
     }
 
@@ -15,3 +17,12 @@ export default class Contador extends Component {
         )
     }
 }
+
+////// Solução 01
+    // constructor( props ) {
+    //    super( props ) 
+    //    this.maisUm = this.maisUm.bind( this )
+    // }
+
+////// Solução 02
+    // <button onClick = { ( ) => this.maisUm } >Incrementa</button>
