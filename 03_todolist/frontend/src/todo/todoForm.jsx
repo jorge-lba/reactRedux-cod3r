b5aafeacc10c5dde39869480e4eb1013e7a46cc6
jorge-lba <jorgeluiz.b.alegretti@gmail.com>
2020-01-11 20:52:24 -0300

    Criando evento onChange

--- 03_todolist/frontend/src/todo/todoForm.jsx
+++ 03_todolist/frontend/src/todo/todoForm.jsx
@@ -6,11 +6,14 @@ export default props => (
     <div role='from' className='todoForm' >
         <Grid cols='12 9 10'>
             <input id="description" className="form-control"
+                onChange={ props.handleChange }
                 placeholder='Adiciona uma tarefa' />    
         </Grid>
         
         <Grid cols='12 3 2'>
-            <IconButton style= 'primary' icon= 'plus' onClick={ props.handleAdd } />
+            <IconButton style= 'primary' icon= 'plus' 
+                onClick={ props.handleAdd }
+                value={ props.description } />
         </Grid>
     </div>       
 
